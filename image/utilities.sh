96bb19537e0cb7a19e1a142d034b5dc8e284c570
Qasim <vs.dev50@gmail.com>
2025-04-09 11:13:23 +0300

    [REMOVE] nginx comment

--- image/utilities.sh
+++ image/utilities.sh
@@ -19,6 +19,3 @@ cp /bd_build/php/custom.ini /usr/local/etc/php/conf.d/custom.ini
 ## Copy caddyfile to location
 mkdir -p /extra_config
 cp /bd_build/caddy/Caddyfile /extra_config/Caddyfile
-
-# Nginx access log on /var/log/access.log only
-# sed -i.bak 's/access.log = \/proc\/self\/fd\/2/access.log = \/proc\/self\/fd\/1/g' /usr/local/etc/php-fpm.d/docker.conf
